16222f1ce0332bbe60fb8a6a8550dfc0786974c7
erguotou <erguotou525@gmail.com>
2017-05-23 18:21:32 +0800

    button fix

--- demo/src/views/Button.vue
+++ demo/src/views/Button.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="v-buttons">
-    <u-button class="button-item">primary</u-button>
+    <u-button class="button-item" @click="toast">primary</u-button>
+    <u-button class="button-item" disabled>primary disabled</u-button>
     <u-button class="button-item" type="highlight">highlight</u-button>
     <u-button class="button-item" type="minor">minor</u-button>
   </div>
 </template>
+<script>
+const modal = weex.requireModule('modal')
+export default {
+  methods: {
+    toast () {
+      modal.toast({
+        message: 'This is a toast',
+        duration: 0.3
+      })
+    }
+  }
+}
+</script>
 <style lang="stylus" scoped>
 .button-item
   margin-top 20px
--- packages/button/Button.vue
+++ packages/button/Button.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="btn" :class="['btn-tp-' + type, 'btn-sz-' + size, disabled ? 'disabled' : '']">
-    <text class="btn-text" :class="['btn-text-' + type]"><slot></slot></text>
+  <div class="btn">
+    <text class="btn-text"
+      :class="['btn-tp-' + type, 'btn-sz-' + size, 'btn-text-' + type, disabled ? ('btn-tpd-' + type) : '']"
+      @click="e => $emit('click', e)"
+      @longpress="e => $emit('longpress', e)">
+      <slot></slot>
+    </text>
   </div>
 </template>
 <script>
@@ -25,23 +30,29 @@ export default {
 <style lang="stylus" scoped>
 @import "../theme-default/var.styl"
 .btn
+  position relative
   align-items center
   justify-content center
-  margin-left auto
-  margin-right auto
+.btn-text
   width 670px
   height 96px
-  text-align center
+  line-height @height
   border-radius 4px
-.btn-text
   font-size 34px
   font-weight 300
+  text-align center
 .btn-tp-primary
   background-color $color-primary
 .btn-tp-highlight
   background-color $color-highlight
 .btn-tp-minor
   background-color $color-btn-minor
+.btn-tpd-primary
+  background-color rgba($color-primary, 0.52)
+.btn-tpd-highlight
+  background-color rgba($color-highlight, 0.52)
+.btn-tpd-minor
+  background-color rgba($color-btn-minor, 0.52)
 .btn-text-primary
   color #fff
 .btn-text-highlight
